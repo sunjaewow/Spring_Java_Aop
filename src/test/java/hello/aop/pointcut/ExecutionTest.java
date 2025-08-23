6e6244cacef37649b0fcea4325cb6422b10ce5fb
sunjaewow <chltjswo789@naver.com>
2025-08-23 17:13:13 +0900

    exactMethod

--- src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -2,14 +2,17 @@ package hello.aop.pointcut;
 
 import hello.aop.member.MemberServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
 import java.lang.reflect.Method;
 
 @Slf4j
 public class ExecutionTest {
 
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
     Method method;
 
     @BeforeEach
@@ -20,5 +23,11 @@ public class ExecutionTest {
     @Test
     public void printMethod() {
         log.info("helloMethod={}", method);
+    }
+
+    @Test
+    public void exactMethod() {
+        pointcut.setExpression("execution(public String hello.aop.member.MemberServiceImpl.hello(String))");
+        Assertions.assertThat(pointcut.matches(method, MemberServiceImpl.class)).isTrue();
     }
 }
